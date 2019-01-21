16b3c833d5630657a33ed73f8caf53575fd33a41
sharkspeed <xiangyuliu2016@outlook.com>
2019-01-22 02:01:05 +0800

    use deploy key

---
+++ .travis-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+# setup ssh-agent and provide the GitHub deploy key
+eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_40b523e6865b_key -iv $encrypted_40b523e6865b_iv -in travis_rsa.enc -out travis_rsa
+chmod 600 travis_rsa
+ssh-add travis_rsa
